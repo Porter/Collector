bb9bb0429cc67c6eecb489722844011dbd33e46b
Porter <pmh192@gmail.com>
2017-10-25 15:46:42 -0700

    /signup returns a JWT

--- src/main/java/com/porter/collector/resources/UserResource.java
+++ src/main/java/com/porter/collector/resources/UserResource.java
@@ -2,13 +2,13 @@ package com.porter.collector.resources;
 
 
 import com.porter.collector.db.UsersDao;
+import com.porter.collector.model.JWTUser;
 import com.porter.collector.model.User;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path(Urls.USER)
-@Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class UserResource {
 
@@ -21,15 +21,17 @@ public class UserResource {
     @POST
     @Path("/signup")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public User create(@FormParam("email") String email,
+    @Produces(MediaType.TEXT_PLAIN)
+    public String create(@FormParam("email") String email,
                            @FormParam("username") String username,
                            @FormParam("password") String password) {
         User user = usersDao.insert(email, username, password);
-        return user;
+        return JWTUser.toJWT(user);
     }
 
     @POST
     @Path("/login")
+    @Produces(MediaType.TEXT_PLAIN)
     public User auth(@FormParam("login") String login, @FormParam("password") String password) {
         return null;
     }
